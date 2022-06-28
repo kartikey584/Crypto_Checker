ba19ee42bc0bd56124fbe885c9a06bda28c3acab
Kartikey-Kesarwani <78426538+Kartikey-Kesarwani@users.noreply.github.com>
2022-06-28 17:17:13 +0530

    css done

--- src/crptoCurrency.js
+++ src/crptoCurrency.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const CryptoCurrency = props => {
     console.log(props);
-    const currency = props.coin.map(({id,name, image,symbol , current_price,market_cap }) =>{
+    const currency = props.coin.map(({id,name, image,symbol , current_price,market_cap,price_change_percentage_24h }) =>{
         return (
-            <div className = 'coins'>
-                <img className = 'coinImage' key = {id} src={image} alt = {symbol} />
-                
+            <div key = {id} className = 'coins'>
+                <img className = 'coinImage'  src={image} alt = {symbol} />
                 <span className = 'coinName'>{name}</span>
-                <span className = 'coinSymbol' >{symbol}</span>
-                <span className = 'coinPrice' >{current_price}</span>
-                <span className = 'coinMktcap'>{market_cap}</span>
-
+                <span className = 'coinSymbol' >{symbol.toUpperCase()}</span>
+                <span className = 'coinPrice' >${current_price}</span>
+                <span className = {price_change_percentage_24h < 0 ? 'red' : 'green' }>{price_change_percentage_24h}%</span>
+                <span className = 'coinMktcap'>Mkt Cap : {market_cap}</span>
+                <button className = 'button'>Know More!</button>
             </div>
         );
     })
